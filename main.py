be48b191db700895c9aa99fe600b4e12aa30ba9f
Kushal <ace.kushal2399@gmail.com>
2025-07-03 18:53:25 +0530

    Add popup with auto-close and formatted label

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import time
+from tkinter import *
+def show_pop_up():
+    root = Tk()
+    root.geometry('600x480+300+100')
+    l = Label(root , text = 'Break Time' , font=("Times New Roman" , 48))
+    l.pack(expand=True)
+    root.after(2000 , root.destroy)
+    root.mainloop()
+
 
 while True:
-    print("Work time... (10 seconds)")
-    time.sleep(10)
-    print("LOOK AWAY! (5 seconds)")
+    print("Work time... (5 seconds)")
     time.sleep(5)
+    print("LOOK AWAY! (2 seconds)")
+    show_pop_up()
+    
+
