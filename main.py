9e536d436d545e94e2982e83a0327bacd4cee171
Kushal <ace.kushal2399@gmail.com>
2025-07-08 15:25:18 +0530

    Refined the timer logic by adding a stop button

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 from tkinter import *
+import threading
+stop_event = threading.Event()
 def show_pop_up():
     root = Tk()
     root.geometry('600x480+300+100')
@@ -8,11 +10,14 @@ def show_pop_up():
     root.after(2000 , root.destroy)
     root.mainloop()
 
+
 def start_break_timer():
-    while True:
+    while not stop_event.is_set():
         print("Work time... (5 seconds)")
         time.sleep(5)
         print("LOOK AWAY! (2 seconds)")
         show_pop_up()
-    
+    print("the loop is stopped")
 
+def stop_timer():
+    stop_event.set()
--- menu_ui.py
+++ menu_ui.py
@@ -1,19 +1,26 @@
+import threading
 from tkinter import *
-from main import start_break_timer
+from main import start_break_timer , stop_timer
 def menu():
     root = Tk()
     root.title("menu")
     root.geometry("600x480")
     Label(root, text="Screen Care Assistant", font=("Arial", 18)).pack(pady=20)
+    def screen_timer():
+        thread = threading.Thread(target=start_break_timer)
+        thread.daemon = True
+        thread.start()
 
-    btn_1 = Button(root, text= "Screen timer", command=start_break_timer)
+    btn_1 = Button(root, text= "Screen timer", command=screen_timer)
     btn_2 = Button(root, text= "Pomodoro timer")
     btn_3 = Button(root, text= "Screen monitor")
     btn_4 = Button(root, text= "Sound")
+    btn_stop = Button(root , text= "stop" , command=stop_timer)
     btn_1.pack(pady=10)
     btn_2.pack(pady=10)
     btn_3.pack(pady=10)
     btn_4.pack(pady=10)
+    btn_stop.pack(padx=10)
     root.mainloop()
 
 menu()
